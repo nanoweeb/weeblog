0d098ec0df0cb0484ca2105a527a79e238b6bd44
Sujaruu <ytfuga02@gmail.com>
2022-08-09 22:18:14 +0700

    bug fixing

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -12,12 +12,16 @@ export async function getServerSideProps() {
   };
 }
 
+function limit(string = "", limit = 240) {
+  return string.substring(0, limit) + "...";
+}
+
 export default function Posts({ data }) {
   return (
     <div className="w-full">
-      <div className="max-w-[1000px] px-5">
+      <div className="max-w-[1000px] px-5 mx-auto">
         <main>
-          <h1 className="text-2xl text-gray-200 font-semibold mb-10">
+          <h1 className="text-2xl text-gray-200 font-semibold mb-10 ">
             Weeblog
           </h1>
           <article>
@@ -25,18 +29,15 @@ export default function Posts({ data }) {
               return (
                 <div
                   key={p.id}
-                  className="h-32 flex justify-between shadow-md mb-6 p-5 border-l-4 border-[#066163]"
+                  className="shadow-md space-y-6 mb-6 p-5 border-l-4 border-[#066163]"
                 >
-                  <div>
-                    <h1 className="text-lg font-semibold text-gray-200">
-                      {p.title}
-                    </h1>
-                    <p className="text-xs text-gray-400">{p.content}</p>
-                    <Link href={"/posts/detail/" + p.endpoint}>
-                      <a className="text-gray-200 text-sm">Read More</a>
-                    </Link>
-                  </div>
-                  <div className="w-20 bg-slate-300 rounded-xl">image</div>
+                  <h1 className="text-lg font-semibold text-gray-200 ">
+                    {p.title}
+                  </h1>
+                  <p className="text-gray-400 ">{limit(p.content)}</p>
+                  <Link href={"/posts/detail/" + p.endpoint}>
+                    <a className="text-gray-200 text-xs">Read More</a>
+                  </Link>
                 </div>
               );
             })}
