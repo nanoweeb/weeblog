0ef4669d97832a522da1aa1e33cac1c306f9d607
Sujaruu <ytfuga02@gmail.com>
2022-08-11 19:03:02 +0700

    update style

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
+import { MdOutlineClose } from "react-icons/md";
 import { BsFillMoonFill } from "react-icons/bs";
 import { MdWbSunny } from "react-icons/md";
 
@@ -11,17 +12,17 @@ export default function Navbar() {
   }
 
   return (
-    <div className="py-4 border-b-3 border-gray-400 px-5 bg-slate-500">
+    <div className="py-4 border-b-3 px-5 bg-[#0F172A]">
       <nav>
-        <button onClick={handlerToggle}>
-          <HiMenuAlt4 />
+        <button onClick={handlerToggle} className="text-2xl">
+          {isOpen ? <MdOutlineClose /> : <HiMenuAlt4 />}
         </button>
         {isOpen && (
-          <div className="absolute top-0 right-0 mt-2 mr-2">
-            <ul>
-              <li>post</li>
-              <li>dashboard</li>
-              <li>profile</li>
+          <div className="h-screen w-full text-xl text-gray-200 absolute top-10 left-0 px-5 py-10 bg-[#0F172A]">
+            <ul className="space-y-5">
+              <li className="border-b-[1px] py-1 border-gray-500">post</li>
+              <li className="border-b-[1px] py-1 border-gray-500">dashboard</li>
+              <li className="border-b-[1px] py-1 border-gray-500">profile</li>
             </ul>
           </div>
         )}
