2755882132b83fff5c51a1a086417dd63ca9a766
zul id <storyciks@gmail.com>
2022-08-12 08:02:58 +0700

    buton

--- pages/posts/create.js
+++ pages/posts/create.js
@@ -43,7 +43,7 @@ export default function create() {
   }
 
   return (
-    <div className="w-full bg-[#0F172A]">
+    <div className="w-full bg-[#111e3b] ">
       <main className="max-w-[1000px] px-5 mx-auto pt-6 ">
         <h1 className="bg-gradient-to-r bg-clip-text text-transparent from-[#24a4a7] to-indigo-600 text-2xl font-semibold mb-10">
           Create a new posts
@@ -67,7 +67,7 @@ export default function create() {
 
           <button
             type="submit"
-            className="py-2 text-gray-200 rounded-lg bg-sky-600"
+            className="py-2 text-gray-200 rounded-lg bg-sky-600 w-fit px-12"
           >
             send
           </button>
