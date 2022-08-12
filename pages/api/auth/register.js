1f810ff45e1a7d7f5d1e5b261781437a681f4420
zul id <storyciks@gmail.com>
2022-08-12 09:45:11 +0700

    reg

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -3,12 +3,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") return req.status(401).end();
+  if (req.method !== "POST") return res.status(401).end();
 
   const data = req.body;
   const register = await prisma.User.create({
     data,
   });
 
-  res.json(200);
+  res.status(200)
+  res.json({
+    message: "register berhasil",
+    data
+  })
 }
--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,21 +1,94 @@
-import { PrismaClient } from "@prisma/client";
+import React from "react";
+import Router from "next/router";
 
-const prisma = new PrismaClient();
 
 export default function register() {
-  async function register() {}
+
+  const [data, setData] = React.useState({
+    username: "",
+    email: "",
+    password: ""
+  })
+
+  async function register(e) {
+    e.preventDefault()
+
+    const createReq = await fetch("/api/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    const createRes = await createReq.json()
+
+    console.log("register berhasil")
+    Router.push("/posts/create")
+
+    
+  }
+
+
+
+  function fromHandler(e) {
+    e.preventDefault()
+
+    const name = e.target.name
+    const value = e.target.value
+
+    setData({
+      ...data,
+      [name]: value
+      
+
+    })
+    
+  }
+
+
+  
   return (
-    <div>
-      <h1>register</h1>
-      <form>
-        <input type="text" placeholder="username" />
-        <br />
-        <input type="text" placeholder="email" />
-        <br />
-        <input type="text" placeholder="password" />
-        <br />
-        <button>Register</button>
-      </form>
+    <div className="bg-black">
+      <div className="bg-slate-800 container mx-auto">
+        <form
+          onSubmit={register}
+          
+        >
+
+        <h2 className="text-2xl text-white">Register</h2>
+          <input
+            name= "username"
+            type="text"
+            placeholder="username"
+            className="border-2 border-white rounded-sm"
+            onChange={fromHandler}
+
+          /> <br />
+          <input
+            name= "email"
+            type="text"
+            placeholder="email"
+            className="border-2 border-white rounded-sm"
+            onChange={fromHandler}
+
+          /> <br />
+          <input
+            name= "password"
+            type="text"
+            placeholder="password"
+            className="border-2 border-white rounded-sm"
+            onChange={fromHandler}
+
+          /> <br />
+        
+          <button type="submit"
+            className="text-white bg-blue-600 px-6 m-4 rounded-sm"
+
+            
+          >Register</button>
+        </form>
+         
+      </div>
+
     </div>
   );
 }
