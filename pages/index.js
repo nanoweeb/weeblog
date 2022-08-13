abf4732b2e40bedf688e2b7351cedf8db7e34ee4
Sujaruu <ytfuga02@gmail.com>
2022-08-13 18:45:10 +0700

    up code

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,11 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <div className="w-full text-gray-200 bg-[#0F172A]">
+      <div className="w-full h-screen text-gray-200 bg-[#0F172A]">
         <main className="max-w-[1000px] mx-auto px-5 py-10">
           <h1>welcome</h1>
-          <br />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
-            optio obcaecati tempora repellat ad corrupti. Porro cum blanditiis
-            ut suscipit?
-          </p>
-          <br />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ex
-            veritatis, quia incidunt voluptatum, delectus labore nobis dolore
-            fuga temporibus magnam sed rerum! Rerum facilis minus earum
-            obcaecati voluptas similique. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Aspernatur recusandae iusto eos voluptatum. Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            suscipit nesciunt necessitatibus deserunt quos libero aperiam neque
-            nihil nisi minus?
-          </p>
+
+          <h1>halaman untuk post public</h1>
         </main>
       </div>
     </Layout>
