75758598add7e4a34090a342135a031e08d5ffca
zul id <storyciks@gmail.com>
2022-09-06 13:03:57 +0700

    styling

--- pages/index.js
+++ pages/index.js
@@ -31,24 +31,19 @@ export default function Home({ posts }) {
         <main className="max-w-[1000px] h-full text-gray-200 bg-[#0F172A] pt-20 px-5 mx-auto">
           {/* featured post */}
           <h2 className="text-2xl text-gray-400 font-bold">Recomendation</h2>
-          <article className="flex justify-between mt-5 mb-20 gap-5">
-            <div className="flex justify-center item-center h-80 w-full rounded-lg bg-black overflow-hidden">
-              <Image
-                src="/nanoweeb.png"
-                width="300px"
-                height="300px"
-                alt="hutao"
-              />
+          <article className="flex flex-col lg:flex-row  mt-5 mb-20 gap-5">
+            <div className="flex justify-center item-center  max-h-[300px] w-full md:w-1/2 rounded-lg overflow-hidden">
+              <Image src="/asd.png" width="1000px" height="500" alt="hutao" />
             </div>
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4 w-full md:w-1/2">
               <p className="text-gray-400">
                 Gaming <span className="px-2">•</span> 31 August 2022
               </p>
-              <h1 className="w-72 text-xl font-semibold">
+              <h1 className=" text-xl font-semibold">
                 Understanding color theory: the color wheel and finding
                 complementary colors
               </h1>
-              <p className="w-72 text-gray-400">
+              <p className=" text-gray-400">
                 Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
                 ullamco cillum dolor. Voluptate exercitation incididunt aliquip
                 deserunt reprehenderit elit laborum.
