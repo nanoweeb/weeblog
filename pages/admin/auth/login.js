f0d0022b3b6bfabe096b7ac9f0085e6dc4703dbf
Sujaruu <ytfuga02@gmail.com>
2022-09-09 20:35:00 +0700

    styling

--- pages/admin/auth/login.js
+++ pages/admin/auth/login.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import cookie from "js-cookie";
 import Router from "next/router";
 import Link from "next/link";
+import Layout from "../../../components/Layout";
 
 export default function Login() {
   const [data, setData] = useState({
@@ -40,34 +41,39 @@ export default function Login() {
   }
 
   return (
-    <div className="bg-black">
-      <div className="bg-slate-800 container mx-auto">
+    <Layout>
+      <div className="w-full h-screen flex justify-center items-center bg-[#131d35]">
         <form onSubmit={submitHandler}>
-          <h2 className="text-2xl text-white">Login</h2>
-          <input
-            name="email"
-            type="text"
-            placeholder="email"
-            className="border-2 border-white rounded-sm"
-            onChange={formHandler}
-          />
-          <br />
-          <input
-            name="password"
-            type="text"
-            placeholder="password"
-            className="border-2 border-white rounded-sm"
-            onChange={formHandler}
-          />
-          <br />
-          <button
-            type="submit"
-            className="text-white bg-blue-600 px-6 m-4 rounded-sm"
-          >
-            Login
-          </button>
+          <div className="text-gray-200 mb-10">
+            <h1 className="text-5xl">Welcome Back Folks 🎉</h1>
+            <p>if you don't have account try to sign-up</p>
+          </div>
+          {/* input area */}
+          <div className="flex flex-col gap-2">
+            <input
+              name="email"
+              type="text"
+              placeholder="email"
+              className="w-full p-2 rounded-md"
+              onChange={formHandler}
+            />
+            <input
+              name="password"
+              type="text"
+              placeholder="password"
+              className="w-full p-2 rounded-md"
+              onChange={formHandler}
+            />
+            <p className="text-pink-600">Forget password ?</p>
+            <button
+              type="submit"
+              className="p-2 text-white bg-blue-600 rounded-md"
+            >
+              Login
+            </button>
+          </div>
         </form>
       </div>
-    </div>
+    </Layout>
   );
 }
