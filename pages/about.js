511d04c8543da54415e148b159ee93f5414607ff
Sujaruu <ytfuga02@gmail.com>
2022-09-15 20:14:01 +0700

    add about

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,13 @@
+import Layout from "../components/Layout";
+
 export default function about() {
-  return <div>about</div>;
+  return (
+    <Layout>
+      <div className="w-full bg-[#0F172A] pb-24">
+        <div className="max-w-[1000px] h-screen text-gray-200 bg-[#0F172A] pt-20 px-5 mx-auto">
+          <h1>about</h1>
+        </div>
+      </div>
+    </Layout>
+  );
 }
